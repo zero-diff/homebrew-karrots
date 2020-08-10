eb9cfd5111b20dffa30e4c793ee99fb163ff8231
Jeff Williams <jeff.williams@gadgetworks.com>
2020-08-10 11:32:09 -0700

    #1 fix the release sha256

--- Formula/karrots.rb
+++ Formula/karrots.rb
@@ -2,7 +2,7 @@ class Karrots < Formula
   desc "creates fully gitops-automated kubernetes clusters"
   homepage "https://zero-diff.github.io/karrots"
   url "https://zero-diff.github.io/karrots/releases/binaries/brew/karrots.tar.gz"
-  sha256 "55030ef5a7aebae211b1dd970751508bb0a1a5d8d001919d3106bb5b67cf17ea"
+  sha256 "3b27b78133bab8999150057133ebcd74a321516356aa6d8142684ff1d8c1e8a5"
   version "0.1.0"
 
   bottle :unneeded
