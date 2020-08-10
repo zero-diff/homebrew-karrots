a6eca89df6c59ae09e2be2621998455b0c208512
Jeff Williams <jeff.williams@gadgetworks.com>
2020-08-10 11:10:06 -0700

    #1 add tap formula that gets binaries from karrots main docs repo

---
+++ Formula/karrots.rb
@@ -0,0 +1,13 @@
+class Karrots < Formula
+  desc "creates fully gitops-automated kubernetes clusters"
+  homepage "https://zero-diff.github.io/karrots"
+  url "https://zero-diff.github.io/karrots/releases/binaries/brew/latest/karrots.tar.gz"
+  sha256 "55030ef5a7aebae211b1dd970751508bb0a1a5d8d001919d3106bb5b67cf17ea"
+  version "0.1.0"
+
+  bottle :unneeded
+
+  def install
+    bin.install "karrots"
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+## Karrots Homebrew Tap Repo
+
+This repo exists to publish the Karrots homebrew tap.
+
+To install Karrots execute this command:
+
+```
+> brew install zero-diff/karrots
+```
