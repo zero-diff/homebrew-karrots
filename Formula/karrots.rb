b641a08e6e8b62c4fec48cfbce041bfe53781e58
Jeff Williams <jeff.williams@gadgetworks.com>
2020-09-14 22:25:23 -0700

    update the karrots binary sha256

--- Formula/karrots.rb
+++ Formula/karrots.rb
@@ -2,7 +2,7 @@ class Karrots < Formula
   desc "creates fully gitops-automated kubernetes clusters"
   homepage "https://zero-diff.github.io/karrots"
   url "https://zero-diff.github.io/karrots/releases/binaries/brew/karrots.tar.gz"
-  sha256 "895f92f50d461c798972abfb46bffb588531d57e39f0c484b97c10050febabf1"
+  sha256 "c47e1b38fcb49c3f3196dc8397372509fb6dcb9dba769619ce751d3de0ed9119"
   version "0.1.0"
   
   depends_on "git"
