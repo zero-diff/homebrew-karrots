a93ac6343e3d7ef07b7acc6b16a5a69d00fd98ad
Jeff Williams <jeff.williams@gadgetworks.com>
2020-10-08 20:27:38 -0700

    make the formula repo a little more compliant (license, version, etc.)

--- Formula/karrots.rb
+++ Formula/karrots.rb
@@ -1,11 +1,13 @@
 class Karrots < Formula
   desc "creates fully gitops-automated kubernetes clusters"
-  homepage "https://zero-diff.github.io/karrots"
+  homepage "https://karrots.zerodiff.org"
+  license "BSD-3-Clause"
   url "https://karrots.zerodiff.org/releases/binaries/brew/karrots.tar.gz"
   sha256 "239654d94aa43ca8574a4a8893ff025f9e61fd2cff66c88709e3e8e419f5ab59"
   version "0.2.0"
   
   depends_on "git"
+  depends_on "jq"
 
   bottle :unneeded
 
