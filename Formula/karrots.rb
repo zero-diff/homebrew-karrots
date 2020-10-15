f599f11faa32b1d630b9d372bf9497a82e44b9b8
Jeff Williams <jeff.williams@gadgetworks.com>
2020-10-15 09:55:28 -0700

    resolves #4 upgrade to version 0.2.3

--- Formula/karrots.rb
+++ Formula/karrots.rb
@@ -3,8 +3,8 @@ class Karrots < Formula
   homepage "https://karrots.zerodiff.org"
   license "BSD-3-Clause"
   url "https://karrots.zerodiff.org/releases/binaries/brew/karrots.tar.gz"
-  sha256 "24ac12b4bfb04bd6d8fa9a904fb48fdab3d868e6828ba5a6bedd0b1b527b7f6e"
-  version "0.2.2"
+  sha256 "ca37a82b72c755bc2ea88c4f1b60a764eaa066586fd313101f8e67399fc91280"
+  version "0.2.3"
   
   depends_on "git"
   depends_on "jq"
