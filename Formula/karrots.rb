d67776b60350a24a678391205463fdf68cb664ca
Jeff Williams <jeff.williams@gadgetworks.com>
2020-11-02 15:51:03 -0800

    release version 0.2.4

--- Formula/karrots.rb
+++ Formula/karrots.rb
@@ -3,7 +3,7 @@ class Karrots < Formula
   homepage "https://karrots.zerodiff.org"
   license "BSD-3-Clause"
   url "https://karrots.zerodiff.org/releases/binaries/brew/karrots.0.2.4-1.tar.gz"
-  sha256 "4616aa36cce1fb3a6370ccc0c8a963df8aa298e5321564b0e7a72ac1255ab9cc"
+  sha256 "c0823beb0729c3d0b4393928b7fb28d89c06d31a561a942cd7ac561bd31f72c8"
   version "0.2.4"
   
   depends_on "git"
