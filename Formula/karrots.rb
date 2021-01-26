14e1ddbe72cf95a6284c2213a9f40a3640b736c5
Jeff Williams <jeff.williams@gadgetworks.com>
2021-01-25 20:58:51 -0800

    updates for release 0.2.7
    resolves #8

--- Formula/karrots.rb
+++ Formula/karrots.rb
@@ -2,8 +2,8 @@ class Karrots < Formula
   desc "creates fully gitops-automated kubernetes clusters"
   homepage "https://karrots.zerodiff.org"
   license "BSD-3-Clause"
-  url "https://karrots.zerodiff.org/releases/binaries/brew/karrots.0.2.6-1.tar.gz"
-  sha256 "fdddb02e6371fc0e91c49823d58fb68f8f3d1b2538353ddbe7a0569b9011afbc"
+  url "https://karrots.zerodiff.org/releases/binaries/brew/karrots.0.2.7-1.tar.gz"
+  sha256 "618510d44e0912ceadc2314977185101179e51d7dc44de8ba5a61b0700caeda0"
   version "0.2.6"
   
   depends_on "git"
