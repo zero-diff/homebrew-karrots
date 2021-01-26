ccbaec7f01f5fe4d01c935e983ed05db685bc4a4
Jeff Williams <jeff.williams@gadgetworks.com>
2021-01-25 21:12:26 -0800

    fixes 0.2.7 release

--- Formula/karrots.rb
+++ Formula/karrots.rb
@@ -4,7 +4,7 @@ class Karrots < Formula
   license "BSD-3-Clause"
   url "https://karrots.zerodiff.org/releases/binaries/brew/karrots.0.2.7-1.tar.gz"
   sha256 "618510d44e0912ceadc2314977185101179e51d7dc44de8ba5a61b0700caeda0"
-  version "0.2.6"
+  version "0.2.7"
   
   depends_on "git"
   depends_on "jq"
